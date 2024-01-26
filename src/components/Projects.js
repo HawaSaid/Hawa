92b0cc88befae7dbe0c26e73c0fbcb69f813795d
Hawa-Afnane Said <hawa21244@gmail.com>
2024-01-26 16:21:30 -0500

    added some skills, trying to figure out why the tabs are not working anymore in the project.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,7 +3,6 @@ import projImg1 from "../assets/img/Pet_Adoption_Website.png";
 import projImg2 from "../assets/img/Real_Estate_System.png";
 import projImg3 from "../assets/img/project-img3.png";
 import ProjectCards from "../components/ProjectCards";
-import TrackVisibility from "react-on-screen";
 
 export const Projects = () => {
     const projects = [
@@ -11,17 +10,17 @@ export const Projects = () => {
             title: "Pet Adoption Website",
             description: "Design & Development",
             imgUrl: projImg1,
-        },
-        {
+          },
+          {
             title: "Real Estate SYstem",
             description: "Design & Development",
             imgUrl: projImg2,
-        },
-        {
+          },
+          {
             title: "Business Startup",
             description: "Design & Development",
             imgUrl: projImg3,
-        },
+          },
 
     ];
     return (
@@ -29,47 +28,43 @@ export const Projects = () => {
             <Container>
                 <Row>
                     <Col>
-                        <TrackVisibility>
-                            {({ isVisible }) =>
-                                <div className={isVisible ? "animated_animated animate_fadeIn" : ""}>
-                                    <h2> Projects </h2>
-                                    <br />
-                                    <p>Here are all the projects I worked on so far.</p>
-                                </div>}
-                        </TrackVisibility>
+                        <h2> Projects </h2>
+                        <br/>
+                        <p>In this project section, the projects i worked on these past few semesters are being displayed and showcase my talent as a frontend developper and my interest for coding</p>
+
                         <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                            <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                                <Nav.Item>
-                                    <Nav.Link eventKey="first">Tab One</Nav.Link>
-                                </Nav.Item>
-                                <Nav.Item>
-                                    <Nav.Link eventKey="second">Tab Two</Nav.Link>
-                                </Nav.Item>
-                                <Nav.Item>
-                                    <Nav.Link eventKey="third">
-                                        Tab Three
-                                    </Nav.Link>
-                                </Nav.Item>
-                            </Nav>
-                            <br />
-                            <Tab.Content>
-                                <Tab.Pane eventKey="first">
-                                    <Row>
-                                        {
-                                            projects.map((project, index) => {
-                                                return (
-                                                    <ProjectCards
-                                                        key={index}
-                                                        {...project}
-                                                    />
-                                                )
-                                            })
-                                        }
-                                    </Row>
-                                </Tab.Pane>
-                                <Tab.Pane eventKey="second"> Lorem Ipsum</Tab.Pane>
-                                <Tab.Pane eventKey="third"> Lorem Ipsum</Tab.Pane>
-                            </Tab.Content>
+                        <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items">
+                            <Nav.Item>
+                                <Nav.Link eventKey="first">Tab One</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="second">Tab Two</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="third">
+                                    Tab Three
+                                </Nav.Link>
+                            </Nav.Item>
+                        </Nav>
+                        <br/>
+                        <Tab.Content>
+                            <Tab.Pane eventKey="first">
+                                <Row>
+                                    {
+                                        projects.map((project,index) =>{
+                                            return(
+                                                <ProjectCards
+                                                key={index}
+                                                {...project}
+                                                />
+                                            )
+                                        })
+                                    }
+                                </Row>
+                            </Tab.Pane>
+                            <Tab.Pane eventKey="second"> Lorem Ipsum</Tab.Pane>
+                            <Tab.Pane eventKey="third"> Lorem Ipsum</Tab.Pane>
+                        </Tab.Content>
                         </Tab.Container>
                     </Col>
                 </Row>
